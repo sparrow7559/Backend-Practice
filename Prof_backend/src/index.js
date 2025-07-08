3c575e85483dc3ba9393f46f083d19eda59e5da8
sparrow7559 <virajgupta2604@gmail.com>
2025-07-09 00:29:05 +0530

    connect to mongo atlas

---
+++ Prof_backend/src/index.js
@@ -0,0 +1,29 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERR: " + error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// })();
